3aaa1a806e6bdcda2e4a2d6b0be1a1c6beff633f
Mateus de Luna <mateussdl@hotmail.com>
2022-07-08 22:13:21 -0300

    Refactoring imageFetch

---
+++ src/API/imageFetch.js
@@ -0,0 +1,3 @@
+import Axios from 'axios';
+
+export const imageFetch = () => {Axios.get('https://api.thedogapi.com/v1/images/search?limit=25&page=1&order=Desc&api_key=0dd5931e-dc53-4219-9bfb-51274aef6d80').then((response) => {console.log('images:', response)});}
