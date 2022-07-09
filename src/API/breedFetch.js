5d52d12f827be00daa550e669f3cf2f3bd51b1a6
Mateus de Luna <mateussdl@hotmail.com>
2022-07-08 22:13:00 -0300

    Creating breedFetch request

---
+++ src/API/breedFetch.js
@@ -0,0 +1,3 @@
+import Axios from 'axios';
+
+export const breedFetch = () => {Axios.get('https://api.thedogapi.com/v1/breeds?limit=25&page=1&order=Desc&api_key=0dd5931e-dc53-4219-9bfb-51274aef6d80').then((response) => {console.log('breeds:', response)});}
