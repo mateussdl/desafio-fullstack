4ddbfbf9430d1f44359e155dbe8b5cad0b4667fb
Mateus de Luna <mateussdl@hotmail.com>
2022-07-10 17:09:41 -0300

    Addding title and description to DogCards

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Card } from '@mui/material';
-import { CardActions } from '@mui/material/CardActions';
 import { CardContent } from '@mui/material';
 import { CardMedia } from '@mui/material';
-import { Button } from '@mui/material/Button';
-import { Typography } from '@mui/material/Typography';
+import { Typography } from '@mui/material';
 import './DogCard.css';
 
 export const DogCard = () => {
@@ -17,6 +15,14 @@ export const DogCard = () => {
                     image="https://melhoramigo.dog/wp-content/uploads/2016/02/DOGUE-ALEMAO.jpg"
                     alt="Black Great Dane"
                 />
+                <CardContent/>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Black Great Dane
+                    </Typography>
+                <CardContent/>
+                <Typography variant="body2" color="text.secondary">
+                    The Great Dane is a large sized dog breed originating from Germany.
+                </Typography>
             </Card>
         </div>
     )
