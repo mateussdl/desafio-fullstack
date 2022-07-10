a29c752fa6ad7683ec91c1c581b624afed5ed6d9
Mateus de Luna <mateussdl@hotmail.com>
2022-07-10 18:47:24 -0300

    Fixing code

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -9,20 +9,20 @@ export const DogCard = () => {
     return(
         <div>
             <Card className='card1' raised >
-                <CardMedia
+                <CardMedia 
                     component="img"
                     height="140"
                     image="https://melhoramigo.dog/wp-content/uploads/2016/02/DOGUE-ALEMAO.jpg"
                     alt="Black Great Dane"
                 />
-                <CardContent/>
+                <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         Black Great Dane
                     </Typography>
-                <CardContent/>
-                <Typography variant="body2" color="text.secondary">
-                    The Great Dane is a large sized dog breed originating from Germany.
-                </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        The Great Dane is a large sized dog breed originating from Germany.
+                    </Typography>
+                </CardContent>
             </Card>
         </div>
     )
