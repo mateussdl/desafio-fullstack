704793d2be641bf7727b97d4598d7f29bcf91188
Mateus de Luna <mateussdl@hotmail.com>
2022-07-10 16:41:37 -0300

    Adding image to DogCards

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Card } from '@mui/material';
+import { CardActions } from '@mui/material/CardActions';
+import { CardContent } from '@mui/material';
+import { CardMedia } from '@mui/material';
+import { Button } from '@mui/material/Button';
+import { Typography } from '@mui/material/Typography';
 import './DogCard.css';
 
 export const DogCard = () => {
     return(
         <div>
             <Card className='card1' raised >
-                <p>
-                    Isso é um card
-                </p>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://melhoramigo.dog/wp-content/uploads/2016/02/DOGUE-ALEMAO.jpg"
+                    alt="Black Great Dane"
+                />
             </Card>
         </div>
     )
