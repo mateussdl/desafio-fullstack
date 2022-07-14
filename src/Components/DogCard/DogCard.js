d687c2719cc661112736b975f58f9dd774274d47
Mateus de Luna <mateussdl@hotmail.com>
2022-07-13 21:41:51 -0300

    Refactoring DogCard to support images

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -1,29 +1,23 @@
-import React from 'react';
-import { Card } from '@mui/material';
-import { CardContent } from '@mui/material';
-import { CardMedia } from '@mui/material';
-import { Typography } from '@mui/material';
-import './DogCard.css';
+import React from "react";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import "./DogCard.css";
 
-export const DogCard = ({dogName, dogDescription}) => {
-    return(
-        <div>
-            <Card className='card1' raised >
-                <CardMedia 
-                    component="img"
-                    height="140"
-                    image="https://melhoramigo.dog/wp-content/uploads/2016/02/DOGUE-ALEMAO.jpg"
-                    alt="Black Great Dane"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {dogName}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {dogDescription}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </div>
-    )
+export const DogCard = ({ dogName, dogImage }) => {
+  return (
+    <div>
+      <Card className="card1" raised>
+        <CardMedia
+          component="img"
+          height="140"
+          image={dogImage}
+          alt="Black Great Dane"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {dogName}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
