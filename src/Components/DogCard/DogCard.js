199ba4081f373486ae9898dc64138efba6e2383a
Mateus de Luna <mateussdl@hotmail.com>
2022-07-15 16:01:21 -0300

    Implementing clickable cards functionality

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -1,25 +1,35 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import "./DogCard.css";
 
 export const DogCard = ({ dogName, dogImage }) => {
   return (
     <div>
-      <Card className="card1" raised>
-        <CardMedia
-          component="img"
-          height="140"
-          image={dogImage}
-          alt="Black Great Dane"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {dogName}
-          </Typography>
-        </CardContent>
-        <Link to="/Details">About</Link>
-      </Card>
+      <Link to="/Details">
+        <Card className="card1" raised>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={dogImage}
+              alt="Black Great Dane"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {dogName}
+              </Typography>
+            </CardContent>
+            {/* <Link to="/Details">About</Link> */}
+          </CardActionArea>
+        </Card>
+      </Link>
     </div>
   );
 };
