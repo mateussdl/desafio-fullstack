a35266034206e1c104f7866be5e1ce4a4ef4c161
Mateus de Luna <mateussdl@hotmail.com>
2022-07-15 13:52:06 -0300

    Creating "Details.js" file

---
+++ src/pages/Details.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Details = (dog) => {
+  return (
+    <div>
+      <h1>bred_for</h1>
+      <p>{dog.bred_for}</p>
+      <h1>breed_group</h1>
+      <h1>height</h1>
+      <h1>id</h1>
+      <h1>image</h1>
+      <h1>life_span</h1>
+      <h1>name</h1>
+      <h1>reference_image_id</h1>
+      <h1>temperament</h1>
+      <h1>weight</h1>
+    </div>
+  );
+};
