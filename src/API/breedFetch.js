ab6c072794fee9a7da1d23698b6097023cb20f07
Mateus de Luna <mateussdl@hotmail.com>
2022-07-17 02:05:29 -0300

    Increasing the number of the dogs

--- src/API/breedFetch.js
+++ src/API/breedFetch.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const breedFetch = async (setState) => {
  await Axios.get(
-    "https://api.thedogapi.com/v1/breeds?limit=5&page=1&order=Desc&api_key=0dd5931e-dc53-4219-9bfb-51274aef6d80"
+    "https://api.thedogapi.com/v1/breeds?limit=25&page=1&order=Desc&api_key=0dd5931e-dc53-4219-9bfb-51274aef6d80"
   ).then((response) => {
     setState(response.data);
   });
