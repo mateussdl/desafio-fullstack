f845d9033bc5b6707c55088af4c59bc5c782a89b
Mateus de Luna <mateussdl@hotmail.com>
2022-07-18 00:10:32 -0300

    Deleting "imageFetch.js"

--- src/API/imageFetch.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-
-export const imageFetch = async (setState) => {
- await Axios.get(
-    "https://api.thedogapi.com/v1/images/search?limit=5&page=1&order=Desc&api_key=0dd5931e-dc53-4219-9bfb-51274aef6d80"
-  ).then((response) => {
-    setState(response.data);
-  });
-};
