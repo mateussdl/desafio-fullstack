ee5e96b79680720dc1ec73079b3f33e2008bd6f4
Mateus de Luna <mateussdl@hotmail.com>
2022-07-18 00:11:25 -0300

    Refactoring "DogCard.js"

--- src/Components/DogCard/DogCard.js
+++ src/Components/DogCard/DogCard.js
@@ -6,12 +6,11 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-// import { Link } from "react-router-dom";
 import "./DogCard.css";
 
 export const DogCard = ({ dogName, dogImage, dogLife, action }) => {
   return (
-    <Card className="card1" raised>
+    <Card className="dogCard" raised>
       <CardActionArea onClick={action}>
         <CardMedia
           component="img"
