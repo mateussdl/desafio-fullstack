5aea5bf042ca2b486b7d5feef4c4f7e9c96d33ec
Mateus de Luna <mateussdl@hotmail.com>
2022-07-18 00:10:03 -0300

    Removing commented codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, /*Link*/ } from "react-router-dom";
+import { Routes, Route, } from "react-router-dom";
 import "./App.css";
 import { Container } from "@mui/material";
 import { Home, Details } from "./pages";
